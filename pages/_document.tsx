1c2b1a6b9f7f428a610878cc5b53e3f888d7e898
ohara <kzAme79@gmail.com>
2024-09-27 21:05:58 +0900

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body className="antialiased min-h-[100vh] bg-gradient-to-br from-indigo-950 via-indigo-700 to-rose-700">
         <Main />
         <NextScript />
