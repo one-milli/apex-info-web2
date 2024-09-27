00db45fc6d3d010e2ff7689c23ede176d699b138
ohara <kzAme79@gmail.com>
2024-09-27 20:58:22 +0900

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="app">
+      <Head>
+        <title>Apex Legends Map Rotation</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
