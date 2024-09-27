ccd573f2ffb374552b4f3961541f06f451ef2b17
ohara <kzAme79@gmail.com>
2024-09-27 21:34:21 +0900

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </Head>
       <body className="antialiased min-h-[100vh] bg-gradient-to-br from-indigo-950 via-indigo-700 to-rose-700">
         <Main />
