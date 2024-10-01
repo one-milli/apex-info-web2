8f2e385a766adfe5c84a2db3604ec751c6eb1a19
ohara <kzAme79@gmail.com>
2024-10-02 01:01:07 +0900

    add analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { Analytics } from "@vercel/analytics/react";
 import MapRotation from "@/components/MapRotation";
+
 export default function Home() {
   return (
     <>
+      <Analytics />
       <div className="w-full mx-auto mb-8 flex justify-center">
         <MapRotation />
       </div>
